a00872897ec733f5164cba26b6f9b2223df5aa47
Martin Miralles-Cordal <axalon@rutgers.edu>
2019-07-08 13:41:32 -0400

    Updated to 0.6.2 and switched to git clone

--- conanfile.py
+++ conanfile.py
@@ -7,25 +7,23 @@ import os
 
 class ImmerConan(ConanFile):
     name = "immer"
-    version = "0.5.0"
+    version = "0.6.2"
     url = "https://github.com/slepasteur/conan-immer.git"
     homepage = "https://github.com/arximboldi/immer"
     author = "Simon Lepasteur <slepasteur@gmail.com>"
     description = "Postmodern immutable and persistent data structures for C++"
     license = "MIT"
     exports = ["LICENSE.md"]
-    _source_subfolder = "source_subfolder"
-    
+    _source_folder = "%s-%s" % (name, version)
+
     def source(self):
-        source_url = "https://github.com/arximboldi/immer"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version), sha256="37df55eaa7690865c588512715ac6adfbf94f13fb521d83924b5f38a264bfc34")
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        git = tools.Git(folder=self._source_folder)
+        git.clone("https://github.com/arximboldi/immer.git", branch="v%s" % self.version)
 
     def package(self):
-        self.copy(pattern="LICENSE.txt", dst="licenses", src=self._source_subfolder)
-        self.copy(pattern="*.h", dst=os.path.join("include", "immer"), src=os.path.join(self._source_subfolder, "immer"))
-        self.copy(pattern="*.hpp", dst=os.path.join("include", "immer"), src=os.path.join(self._source_subfolder, "immer"))
+        self.copy(pattern="LICENSE.txt", dst="licenses", src=self._source_folder)
+        self.copy(pattern="*.h", dst=os.path.join("include", "immer"), src=os.path.join(self._source_folder, "immer"))
+        self.copy(pattern="*.hpp", dst=os.path.join("include", "immer"), src=os.path.join(self._source_folder, "immer"))
 
     def package_id(self):
         self.info.header_only()
